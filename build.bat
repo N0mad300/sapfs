14070ff404fa3f2f50b87511f7f49d52f9200abf
N0mad300 <arthur.raffoux@gmail.com>
2025-12-28 15:08:33 +0100

    Updated build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,24 @@
-cl /W4 /O2 /Fe:dist\audio_player.exe /Fo:dist\ src\main.c src\audio_decoder\wave_parser.c src\audio_decoder\audio_decoder.c src\audio_output\audio_output.c src\audio_output\wasapi_output.c src\utils.c ole32.lib
+@echo off
+
+set CC=cl
+set CFLAGS=/W4 /O2
+set OUTDIR=dist
+set OUT=%OUTDIR%\audio_player.exe
+
+set SRC= ^
+    src\main.c ^
+    src\audio_decoder\wave_parser.c ^
+    src\audio_decoder\audio_decoder.c ^
+    src\audio_output\audio_output.c ^
+    src\audio_output\wasapi_output.c ^
+    src\utils.c
+
+set LIBS=ole32.lib
+
+if not exist %OUTDIR% mkdir %OUTDIR%
+
+%CC% %CFLAGS% ^
+    /Fe:%OUT% ^
+    /Fo:%OUTDIR%\ ^
+    %SRC% ^
+    %LIBS%
